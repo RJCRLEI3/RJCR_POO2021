de41346ac89a5770d69e7cf41e19200f20f5c5be
Ricardo Rocha <160221080@estudantes.ips.pt>
2021-03-04 17:59:40 +0000

    Class AppStart

---
+++ AppStart.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class AppStart here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class AppStart
+{
+    public static void main() {
+        Size size = Size.MEDIUM;
+        System.out.println("Name        :            " + size.name());
+        System.out.println("toString()  :            " + size.toString().trim());
+        System.out.println("Ordem       :            " + size.ordinal());
+        System.out.println("Valor mínimo:            " + size.getMinValue());
+        System.out.println("Valor máximo:            " + size.getMaxValue());
+        System.out.println("Código      :            " + size.getCode());
+    }
+}
